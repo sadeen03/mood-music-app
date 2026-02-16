adfc9d7bc4b4dd31984ee183712e2750e9a7acbb
sadeen03 <sadeentaleb7@example.com>
2026-02-17 02:41:21 +0300

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000';
+
+const API_URL = 'https://mood-music-app-nsgs.onrender.com/';
 
 const api = axios.create({
   baseURL: API_URL,
